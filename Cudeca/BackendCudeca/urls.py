c28fa849ebd03e95bdfa941c7181034e42856d8c
TheWizardCat <edu1sagaaviles@gmail.com>
2025-12-17 17:58:17 +0100

    Frontend Modificado

--- Cudeca/BackendCudeca/urls.py
+++ Cudeca/BackendCudeca/urls.py
@@ -21,9 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('eventos.urls')),
+    path('', include('eventos.urls')),  # Replace 'your_app' with your app name
 ]
 
-# For serving media files in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
